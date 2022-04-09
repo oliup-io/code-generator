ef3161715986feb224fc68d7914df2cb5f7817e0
Emile Silas SARE <emile.silas@gmail.com>
2022-04-09 10:26:00 +0100

    created

---
+++ tests/autoload.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Copyright (c) OLIUP <dev@oliup.com>.
+ *
+ * This file is part of the Oliup CodeGenerator package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+require_once \dirname(__DIR__) . '/vendor/autoload.php';
